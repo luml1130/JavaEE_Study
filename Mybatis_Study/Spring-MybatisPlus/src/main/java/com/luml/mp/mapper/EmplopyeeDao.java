bd16bcc8b2f1285604c9f3777a6be34f160d2e4f
鲁孟良 <lumengliang@xjj.com>
2021-02-28 21:32:21 +0800

    mybatis

--- Mybatis_Study/Spring-MybatisPlus/src/main/java/com/luml/mp/mapper/EmplopyeeDao.java
+++ Mybatis_Study/Spring-MybatisPlus/src/main/java/com/luml/mp/mapper/EmplopyeeDao.java
@@ -2,9 +2,11 @@ package com.luml.mp.mapper;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.luml.mp.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author luml
  */
+@Mapper
 public interface EmplopyeeDao extends BaseMapper<Employee> {
 }
--- Mybatis_Study/Spring-MybatisPlus/src/test/com/luml/mp/testMP.java
+++ Mybatis_Study/Spring-MybatisPlus/src/test/com/luml/mp/testMP.java
@@ -1,5 +1,7 @@
 package com.luml.mp;
 
+import com.luml.mp.entity.Employee;
+import com.luml.mp.mapper.EmplopyeeDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +22,33 @@ public class testMP {
 
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private EmplopyeeDao emplopyeeDao;
 
     @Test
     public void testDataSource() throws SQLException {
         System.out.println(dataSource.getConnection());
     }
+
+    @Test
+    public void testInsert(){
+        Employee employee = new Employee();
+        employee.setLastName("东方不败");
+        employee.setEmail("dfbb@163.com");
+        employee.setGender(1);
+        employee.setAge(20);
+        emplopyeeDao.insert(employee);
+        //mybatisplus会自动把当前插入对象在数据库中的id写回到该实体中
+        System.out.println(employee.getId());
+    }
+
+    @Test
+    public void testUpdate(){
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setLastName("更新测试");
+        //emplopyeeDao.updateById(employee);//根据id进行更新，没有传值的属性就不会更新
+        emplopyeeDao.updateAllColumnById(employee);//根据id进行更新，没传值的属性就更新为null
+    }
+
 }
